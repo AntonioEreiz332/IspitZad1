ffed4062b3b9595258e5bed70553ff87630d92d0
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-10-18 08:33:11 +0200

    Add files via upload

---
+++ Ispit1zad-AE/Ispit1zad-AE/Sport.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit1zad_AE
+{
+    class Sport
+    {
+        private string Naziv;
+        private bool IgraLiSeLoptom;
+        private byte BrojIgraca;
+
+
+        public Sport(string naziv)
+        {
+            Naziv = naziv;
+        }
+
+        public Sport(string naziv, bool igraLiSeLoptom)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igraLiSeLoptom;
+        }
+
+        public Sport(string naziv, bool igraLiSeLoptom, byte brojIgraca)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igraLiSeLoptom;
+            BrojIgraca = brojIgraca;
+        }
+
+
+    }
+}
